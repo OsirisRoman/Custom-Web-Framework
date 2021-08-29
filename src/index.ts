9e2a5511fab439c620314d9da038930d74b16736
Osiris Roman <osiris.roman@ulinksolution.com>
2021-08-29 14:46:35 -0500

    User Model through inheritance

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+
+const collection = User.buildUserCollection();
+
+collection.on("change", () => {
+  console.log(collection);
+});
+
+collection.fetch();
