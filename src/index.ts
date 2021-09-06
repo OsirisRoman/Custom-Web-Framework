93603887846f111c232b14013c367bedbb5395b8
Osiris Roman <osiris.roman@ulinksolution.com>
2021-09-06 02:32:22 -0500

    Extra Listing all Users in DB

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const user = User.buildUser({ name: "NAME", age: 20 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById("root");
+users.on("change", () => {
+  const root = document.getElementById("root");
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  userEdit.render();
+users.fetch();
 
-  console.log(userEdit);
-} else {
-  throw new Error("Root element not found");
-}
+// import { User } from "./models/User";
+// import { UserEdit } from "./views/UserEdit";
+
+// const user = User.buildUser({ name: "NAME", age: 20 });
+
+// const root = document.getElementById("root");
+
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+
+//   userEdit.render();
+
+//   console.log(userEdit);
+// } else {
+//   throw new Error("Root element not found");
+// }
 
 // import { Collection } from "./models/Collection";
 // import { User, UserProps } from "./models/User";
